deb33397028d0b0a115170b9e872febed318bbfe
adamspersonaldeveloping <adamspersonaldeveloping@gmail.com>
2023-01-15 12:58:41 +0700

    removed comments from deletePost

--- controllers/posts.js
+++ controllers/posts.js
@@ -54,12 +54,12 @@ module.exports = {
     try {
       // Find post by id
       let post = await Post.findById({ _id: req.params.id });
-      let comment = await Comment.findById({ _id: req.params.id });
+      
       // Delete image from cloudinary
       await cloudinary.uploader.destroy(post.cloudinaryId);
       // Delete post from db
       await Post.remove({ _id: req.params.id });
-      await Comment.remove({ _id: req.params.id });
+      
       console.log("Deleted Post");
       res.redirect("/profile");
     } catch (err) {
